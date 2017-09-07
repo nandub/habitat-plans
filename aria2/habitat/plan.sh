9d8b1e6cded42ca24e02366a66bfe229bde65dbb
Fernando Ortiz <fortiz2k@gmail.com>
2017-09-07 13:49:13 -0500

    add nandub/c-ares pkg deps to nandub/aria2

--- aria2/habitat/plan.sh
+++ aria2/habitat/plan.sh
@@ -8,6 +8,7 @@ pkg_license=('gplv2')
 pkg_source=https://github.com/aria2/aria2/releases/download/release-${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=af43688a7fa69f27b8d156b932accdaca847964b0853a62e133d8d84d2f9d0ca
 pkg_deps=(
+  nandub/c-ares
   core/cacerts
   core/gcc-libs
   core/libxml2
