13dd8fd5168e69beedacb585fe4ab737ddc4f3da
Fernando Ortiz <fortiz2k@gmail.com>
2018-04-06 14:57:20 -0500

    change nandub/c-ares to core/c-ares and other changes

--- aria2/habitat/plan.sh
+++ aria2/habitat/plan.sh
@@ -1,14 +1,16 @@
 pkg_name=aria2
+pkg_distname=$pkg_name
 pkg_origin=nandub
 pkg_version=1.32.0
 pkg_description="aria2 is a lightweight multi-protocol & multi-source, cross platform download utility operated in command-line. It supports HTTP/HTTPS, FTP, SFTP, BitTorrent and Metalink."
 pkg_upstream_url=https://github.com/aria2/aria2
 pkg_maintainer="Fernando Ortiz <nandub+habitat@nandub.info>"
 pkg_license=('gplv2')
-pkg_source=https://github.com/aria2/aria2/releases/download/release-${pkg_version}/${pkg_name}-${pkg_version}.tar.gz
+pkg_source=https://github.com/aria2/aria2/releases/download/release-${pkg_version}/${pkg_distname}-${pkg_version}.tar.gz
 pkg_shasum=af43688a7fa69f27b8d156b932accdaca847964b0853a62e133d8d84d2f9d0ca
+pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(
-  nandub/c-ares
+  core/c-ares
   core/cacerts
   core/gcc-libs
   core/libxml2
