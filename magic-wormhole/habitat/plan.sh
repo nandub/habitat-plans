474edcfc792d74d1d58ca30bbf2f7077eee30f79
Fernando Ortiz <fortiz2k@gmail.com>
2018-04-08 01:01:14 -0500

    add magic-wormhole version 0.10.5

--- magic-wormhole/habitat/plan.sh
+++ magic-wormhole/habitat/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=magic-wormhole
 pkg_origin=nandub
-pkg_version="0.10.4"
+pkg_version="0.10.5"
 pkg_maintainer="Fernando Ortiz <nandub+habitat@nandub.info>"
 pkg_license=('MIT')
 pkg_upstream_url=https://github.com/warner/magic-wormhole
 pkg_source=https://github.com/warner/magic-wormhole/archive/${pkg_version}.tar.gz
-pkg_shasum=8219de23a87082e2499972d2c0b98a522bec9501fe75800ad1f635b39ffb9966
+pkg_shasum=2d44237c7da2f916e8e8a86d77622e0bd0e1ec2770dc126dab657a8c4bed2d07
 pkg_build_deps=(
   core/cacerts
   core/gcc
