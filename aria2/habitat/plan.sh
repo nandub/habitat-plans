5b1a81c180741a392d98dac50ad70b20e867728b
Fernando Ortiz <fortiz2k@gmail.com>
2018-10-14 20:39:41 -0500

    add aria2/aria2-static  version 1.34.0

--- aria2/habitat/plan.sh
+++ aria2/habitat/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=aria2
 pkg_distname=$pkg_name
 pkg_origin=nandub
-pkg_version=1.33.1
+pkg_version=1.34.0
 pkg_description="aria2 is a lightweight multi-protocol & multi-source, cross platform download utility operated in command-line. It supports HTTP/HTTPS, FTP, SFTP, BitTorrent and Metalink."
 pkg_upstream_url=https://github.com/aria2/aria2
 pkg_maintainer="Fernando Ortiz <nandub+habitat@nandub.info>"
 pkg_license=('gplv2')
 pkg_source=https://github.com/aria2/aria2/releases/download/release-${pkg_version}/${pkg_distname}-${pkg_version}.tar.gz
-pkg_shasum=59fb7cadbce11a9511ccff28a9ec6ab58df2be8d0b5021cdb606a56e6289de34
+pkg_shasum=ec4866985760b506aa36dc9021dbdc69551c1a647823cae328c30a4f3affaa6c
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(
   core/c-ares
